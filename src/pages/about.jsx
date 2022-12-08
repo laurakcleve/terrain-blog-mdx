dbc58c555bc4898cde1c62023746f5b3859f564e
Laura <laurakcleve@gmail.com>
2022-12-08 11:43:09 -0700

    add about page

---
+++ src/pages/about.jsx
@@ -0,0 +1,44 @@
+import React from 'react';
+
+import Layout from '../components/Layout';
+
+function About() {
+  return (
+    <Layout>
+      <p>
+        My name is Laura, and I like D&D and tabletop gaming. Building terrain
+        and painting minis is my jam.
+      </p>
+
+      <p>Shoot me a message here:</p>
+
+      <form
+        name="contact"
+        method="POST"
+        data-netlify="true"
+        action="/form-success"
+      >
+        <input type="hidden" name="form-name" value="contact" />
+
+        <label htmlFor="name">
+          <span>Name</span>
+          <input type="text" name="name" />
+        </label>
+
+        <label htmlFor="email">
+          <span>Email address</span>
+          <input type="text" name="email" required />
+        </label>
+
+        <label htmlFor="message">
+          <span>Message</span>
+          <textarea name="message" required />
+        </label>
+
+        <button type="submit">Send</button>
+      </form>
+    </Layout>
+  );
+}
+
+export default About;
