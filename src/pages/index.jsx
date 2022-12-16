744b1168bf88b74545e778630e98308f2139ada3
Laura <laurakcleve@gmail.com>
2022-12-15 17:14:44 -0700

    Fix styling

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -22,7 +22,7 @@ export const pageQuery = graphql`
             featuredImage {
               id
               childImageSharp {
-                gatsbyImageData
+                gatsbyImageData(width: 575)
               }
             }
           }
